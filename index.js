3ffa7bb73655b47f571a75787e1e0b65fd2d48f4
dds git <deeann@stelzl.us>
2022-09-12 10:36:47 -0400

    created prompts and functions for manager

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const { listenerCount } = require('process');
+const { choices } = require('yargs');
+const team = []
 
-// TODO: CODE GOES HERE
+const managerQuestions = [
+    {
+    type: 'input',
+    name: 'name',
+    message: "Please input your full name"
+
+},
+{
+    type: 'input',
+    name: 'ID',
+    message: "Please input your ID"
+
+},
+{
+    type: 'input',
+    name: 'email',
+    message: "Please input your email"
+
+},
+{
+    type: 'input',
+    name: 'officeNumber',
+    message: "Please input your office number"
+
+},
+]
+const menuQuestion = {
+    type: 'list',
+    name: 'menuOption',
+    message: 'What would you like to do next?',
+    choices: ['Add an Engineer', 'Add and Intern', 'Finish']
+
+}
+function promptManager(){
+    inquirer.prompt(managerQuestions).then(({name, id, email, office})=>{
+        const employee = new Manager(name, id, email, office)
+        team.push(employee)
+        promptMenu()
+    
+    })
+}
+function promptMenu(){
+    inquirer.prompt(menuQuestion).then(({menuOption})=> {
+        switch (menuOption) {
+            case 'Add an Engineer' : 
+                promtEngineer()
+                break;
+            case 'Add and Intern' : 
+                promptIntern()
+                break;
+            case  'Finish' : 
+                buildHtml()
+                break;
+    
+        }
+    })
+}
+promptManager()
